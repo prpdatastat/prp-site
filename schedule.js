cfdfc63647a2fe333dfbb5013528783fae751680
Reko <rekorex358@gmail.com>
2024-07-02 21:01:10 +0500

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -13,10 +13,9 @@ listParam.push( {
 
 
 $.ajax({
-    url: 'http://prp-api.vercel.app/schedule',
+    url: 'https://prp-api.vercel.app/schedule',
     type: 'GET',
-    headers : {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0', 'origin': 'http://prp.phf.gop.pk/', 'contentType' : 'application/x-www-form-urlencoded; charset=UTF-8'},
-    
+   
     dataType : 'json',
     success: function (response) {
         console.log(response);
